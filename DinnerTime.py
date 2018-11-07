615c01c6c4e1314da0da33577295601f79b71b73
shellwayne01 <shellwayne01@gmail.com>
2018-11-06 19:46:35 -0500

    Simple classes

---
+++ DinnerTime.py
@@ -0,0 +1,32 @@
+class DinnerTime(object): #change the name to something else
+    """docstring for ."""
+    #Variables
+    philosophers = [] #fill with Philosopher objects
+    forks = []  #filled with string for now. How do we know the order of the forks?
+    idCounter = 0
+
+    def __init__(self, numP):
+        if numP = 5{
+        for person in numP:
+            addPerson()
+            fork = "fork"
+            fork.append(forks)
+        }
+        #start thread execution on all philosophers
+        super(, self).__init__()
+        self.arg = arg
+
+    def addPerson():
+        idCounter++
+        person = Philosopher()
+        person.id = idCounter
+        person.lFork = forks[ ((idCounter*2) - 1) % forks.count()]
+        person.rFork = forks[idCounter] #modulus
+        print ("New Person added to the dining table person %)
+        philosophers.append(person)
+
+    def tableStatus():
+        print("There are %s philosophers at the dining table." % (philosophers.count())
+        for person in Philosophers:
+            print("\nPhilosopher %s: \nstatus = %s\nleft fork= %s\nright fork = %s" %
+            (person.id, person.state, person.lFork, person.rFork))
---
+++ Philosopher.py
@@ -0,0 +1,30 @@
+# Import
+class Philosopher (object):
+    #Variables to define
+    # state - ready, running(eating), waiting, start, done
+    # ID or can get from the thread
+    # priority
+    # lFork
+    # rFork
+
+    """docstring fo Philosopher ."""
+    def __init__(self, arg):
+        id = 0
+        state = none
+        priority = 0
+        lFork = "none"
+        rFork = "none"
+        supe Philosopher , self).__init__()
+        self.arg = arg
+
+    def getLFork():
+        #how to access table forks
+        print(lFork)
+
+    def getRFork():
+        #how to access table forks
+        print(rFork)
+
+    def eat():
+        #how to access table forks
+        print("eating")
