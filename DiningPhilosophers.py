0a90e92ef9321db2673793c1692295ed201a4155
shellwayne01 <shellwayne01@gmail.com>
2018-11-07 12:02:18 -0500

    Updated with threads for python v.2.7

--- DiningPhilosophers.py
+++ DiningPhilosophers.py
@@ -1,4 +1,33 @@
-from DinnerTime import DinnerTime
+import threading
+import random
+import time
+# from DinnerTime import DinnerTime
+# from Philosopher import Philosopher
 
-test = DinnerTime(5)
-test.tableStatus()
+# test = DinnerTime(5)
+# test.tableStatus()
+
+# test = Philosopher(4,0)
+# test.run()
+
+if __name__ == '__main__':
+
+    philosophers = [0 for _ in range(5)]
+    chopSticks = [0 for _ in range(5)]
+
+    for i in range(len(chopSticks)):
+        chopSticks[i] = threading.Lock()
+
+    for i in range(len(philosophers)):
+        leftChopStick = chopSticks[i]
+        rightChopStick = chopSticks[(i+1) % len(chopSticks)]
+
+        if i == len(philosophers) - 1:
+            philosophers[i] = Philosopher(rightChopStick, leftChopStick) #review. Circular array is better
+        else:
+            philosophers[i] = Philosopher(leftChopStick, rightChopStick)
+
+        # t = threading.Thread(target=philosophers[i].run, name=f'Philosopher {i+1}') #target here is a runnable class
+        name = "Philosopher %s" % (i+1)
+        t = threading.Thread(target=philosophers[i].run, name=name)
+        t.start()
--- Philosopher.py
+++ Philosopher.py
@@ -1,20 +1,51 @@
-# Import
-class Philosopher:
-    def Philosopher(self, arg):
-        self.id = 0
-        self.state = "none"
-        self.priority = 0
-        self.lChopstick = "none"
-        self.rChopstick = "none"
+import threading
+import random
+import time
+#class Philosopher:
+    # def Philosopher(self, arg):
+    #     self.id = 0
+    #     self.state = "none"
+    #     self.priority = 0
+    #     self.lChopstick = "none"
+    #     self.rChopstick = "none"
+    #
+    # def getLChop():
+    #     #how to access table Chopsticks
+    #     print(lChopstick)
+    #
+    # def getRChop():
+    #     #how to access table Chopsticks
+    #     print(rChopstick)
+    #
+    # def eat():
+    #     #how to access table Chopsticks
+    #     print("eating")
+#Commented syntax only works in python v.3.6
+class Philosopher(object):
 
-    def getLChop():
-        #how to access table Chopsticks
-        print(lChopstick)
+    def __init__(self, leftChopStick, rightChopStick):
+        self.leftChopStick = leftChopStick  #what data type are these? I was using int
+        self.rightChopStick = rightChopStick
 
-    def getRChop():
-        #how to access table Chopsticks
-        print(rChopstick)
+    def doAction(self, action):
+        # print(f'{threading.current_thread().getName()} {action}')
+        print("Thread:%s\n%s" % (threading.current_thread().getName(), action))
+        time.sleep(random.randint(1, 5)) #why random sleep time for various actions?
 
-    def eat():
-        #how to access table Chopsticks
-        print("eating")
+    def run(self):
+        try:
+            while True: #whats true? self?
+                # self.doAction(f'{time.time()}: Thinking')
+                self.doAction("%s: Thinking" % (time.time()))
+                with self.leftChopStick:
+                    # self.doAction(f'{time.time()}: Picked up left chopstick')
+                    self.doAction("%s: Picked up left chopstick" %(time.time()))
+                    with self.rightChopStick:
+                        # self.doAction(f'{time.time()}: Picked up right chopstick - eating')
+                        # self.doAction(f'{time.time()}: Put down right chopstick')
+                        self.doAction("%s: Picked up right chopstick - eating" %(time.time()))
+                        self.doAction("%s: Put down right chopstick" % (time.time()))
+                    # self.doAction(f'{time.time()}: Put down left chopstick. Back to thinking')
+                    self.doAction("%s: Put down left chopstick. Back to thinking" %(time.time()))
+        except Exception as E:
+            print(E)
